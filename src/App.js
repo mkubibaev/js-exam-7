8502743810540e2b097bedb0c1f5018c703b486a
mkubibaev <mkubibaev@gmail.com>
2018-12-29 18:09:38 +0600

    #3 added calcTotalPrice and addOrder item functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,44 @@ import MenuItem from './components/MenuItem/MenuItem';
 import OrderItem from './components/OrderItem/OrderItem';
 
 class App extends Component {
+    state = {
+        items: [
+            {id: 1, name: 'Hamburger', price: 80, count: 0},
+            {id: 2, name: 'Cheeseburger', price: 90, count: 0},
+            {id: 3, name: 'Tea', price: 30, count: 0},
+            {id: 4, name: 'Cola', price: 40, count: 0},
+        ],
+        totalPrice: 0
+    };
+
+    addItemHandler = (id) => {
+        const items = [...this.state.items];
+
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].id === id) {
+                items[i].count++;
+            }
+        }
+
+        this.setState({items}, this.calcTotalPrice());
+    };
+
+    calcTotalPrice = () => {
+        const items = [...this.state.items];
+        let totalPrice = 0;
+
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].count) {
+                let itemPrice = items[i].count * items[i].price;
+                totalPrice += itemPrice;
+            }
+        }
+
+        this.setState({totalPrice});
+    };
+
     render() {
+        console.log(this.state);
         return (
             <div className="container">
                 <div className="order">
@@ -22,7 +59,14 @@ class App extends Component {
                 <div className="menu">
                     <h3 className="module-title">Menu</h3>
                     <div className="menu-items">
-                        <MenuItem />
+                        {this.state.items.map(item => (
+                            <MenuItem
+                                key={item.id}
+                                name={item.name}
+                                price={item.price}
+                                onAddItem={this.addItemHandler.bind(this, item.id)}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const MenuItem = props => (
-    <div className="menu-item">
+    <div className="menu-item" onClick={props.onAddItem}>
         <div className="menu-item__img">
 
         </div>
         <div className="menu-item__txt">
-            <h4 className="menu-item__title">Hamburger</h4>
-            <span>Price: 40 KGS</span>
+            <h4 className="menu-item__title">{props.name}</h4>
+            <span>Price: {props.price} KGS</span>
         </div>
     </div>
 );
